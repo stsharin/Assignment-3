293abe0a1aba5190357fad22a5b3f2ce7c494d2a
stsharin <stssharin@gmail.com>
2021-01-22 04:48:40 +0600

    final

--- assignment.js
+++ assignment.js
@@ -15,7 +15,7 @@ var toMeter = kilometerToMeter(197);
 
 // Budget Calculator
 function budgetCalculator(watchAmount, phoneAmount, laptopAmount) {
-    if (watchAmount < 0 || phoneAmount <0 || laptopAmount<0) {
+    if (watchAmount < 0 || phoneAmount < 0 || laptopAmount < 0) {
         return "Amount cannot be negative. Please give proper value to see the total price."
     }
     else {
@@ -37,24 +37,30 @@ var price = budgetCalculator(2, 2, 2);
 
 // Finding hotel cost
 var cost = 0;
+var extraDays = 0;
 function hotelCost(days) {
-    if (days<=10){
-        cost = days*100;
+    if (days <= 10) {
+        cost = days * 100;
     }
-    else if (days>=11 && days<=20){
-        d = days-10;
-        cost = (d*80) + 1000;
+    else if (days >= 11 && days <= 20) {
+        extraDays = days - 10;
+        cost = (extraDays * 80) + 1000;
     }
-    else{
-        d = days - 20;
-        cost = (d*50) + 1800;
+    else {
+        extraDays = days - 20;
+        cost = (extraDays * 50) + 1800;
     }
     return cost;
 }
-var totalCost = hotelCost(11);
+var totalCost = hotelCost(25);
 // console.log(totalCost);
 
 // Finding biggest string
-function megaFriend() {
-
+function megaFriend(names) {
+    var max = names[0].length;
+    names.map(v => max = Math.max(max, v.length));
+    result = names.filter(v => v.length == max);
+    return result;
 }
+var longestName = megaFriend(['Sharin', 'Saraban Tasnim', 'STS','Saraban Tasnim Sharin','STSharin']);
+console.log(longestName);
