860feb45962f680d5c80a072eacc7cb3365dc648
stsharin <stssharin@gmail.com>
2021-01-21 23:00:57 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,15 @@
+// github repo link
+
+// Finding kilometer to meter
+function kilometerToMeter(km){
+    var meter = (km * 1000);   // 1 km = 1000 meter
+    return meter;
+}
+
+var toMeter = kilometerToMeter(197);
+console.log(toMeter);
+
+// Budget Calculator
+function budgetCalculator(){
+    
+}
