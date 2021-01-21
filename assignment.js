b79d30ce76f979f407a06bf93b3bb578579b43ae
stsharin <stssharin@gmail.com>
2021-01-22 05:13:47 +0600

    final edit

--- assignment.js
+++ assignment.js
@@ -1,6 +1,6 @@
 // https://github.com/stsharin/Assignment-3
 
-// Finding kilometer to meter
+// 1. Finding kilometer to meter
 function kilometerToMeter(km) {
     if (km < 0) {
         return "Distance cannot be negative.";
@@ -11,12 +11,11 @@ function kilometerToMeter(km) {
     }
 }
 var toMeter = kilometerToMeter(197);
-// console.log(toMeter);
 
-// Budget Calculator
+// 2. Budget Calculator
 function budgetCalculator(watchAmount, phoneAmount, laptopAmount) {
     if (watchAmount < 0 || phoneAmount < 0 || laptopAmount < 0) {
-        return "Amount cannot be negative. Please give proper value to see the total price."
+        return "Amount cannot be negative. Please give proper value to see the total price.";
     }
     else {
         // given prices
@@ -24,43 +23,56 @@ function budgetCalculator(watchAmount, phoneAmount, laptopAmount) {
         var phonePrice = 100;
         var laptopPrice = 500;
 
-        totalWatchPrice = watchPrice * watchAmount;
-        totalPhonePrice = phonePrice * phoneAmount;
-        totalLaptopPrice = laptopPrice * laptopAmount;
+        // multiplying price with amount to get the total price
+        var totalWatchPrice = watchPrice * watchAmount;
+        var totalPhonePrice = phonePrice * phoneAmount;
+        var totalLaptopPrice = laptopPrice * laptopAmount;
 
-        totalPrice = (totalWatchPrice + totalPhonePrice + totalLaptopPrice);
+        // calculating total price
+        var totalPrice = (totalWatchPrice + totalPhonePrice + totalLaptopPrice);
         return totalPrice;
     }
 }
-var price = budgetCalculator(2, 2, 2);
-// console.log(totalPrice);
+var price = budgetCalculator(2, 2, 1);
 
-// Finding hotel cost
+// 3. Finding hotel cost
 var cost = 0;
 var extraDays = 0;
+
 function hotelCost(days) {
-    if (days <= 10) {
-        cost = days * 100;
-    }
-    else if (days >= 11 && days <= 20) {
-        extraDays = days - 10;
-        cost = (extraDays * 80) + 1000;
+    if (days < 0) {
+        return "Days cannot be negative.";
     }
     else {
-        extraDays = days - 20;
-        cost = (extraDays * 50) + 1800;
-    }
-    return cost;
-}
-var totalCost = hotelCost(25);
-// console.log(totalCost);
+        if (days <= 10) {
+            cost = days * 100;
 
-// Finding biggest string
-function megaFriend(names) {
-    var max = names[0].length;
-    names.map(v => max = Math.max(max, v.length));
-    result = names.filter(v => v.length == max);
-    return result;
+        }
+        else if (days >= 11 && days <= 20) {
+            extraDays = days - 10;
+            cost = (extraDays * 80) + 1000;
+        }
+        else {
+            extraDays = days - 20;
+            cost = (extraDays * 50) + 1800;
+
+        }
+        return cost;
+    }
 }
-var longestName = megaFriend(['Sharin', 'Saraban Tasnim', 'STS','Saraban Tasnim Sharin','STSharin']);
-console.log(longestName);
+var totalCost = hotelCost(11);
+
+// 4. Finding biggest string
+function megaFriend(names) {
+    // checking if it is an empty array
+    if (names.length === 0) {
+        return "This is an empty array."
+    }
+    else {
+        var max = names[0].length;
+        names.map(v => max = Math.max(max, v.length));
+        maxValue = names.filter(v => v.length == max);
+        return maxValue;
+    }
+}
+var longestName = megaFriend(['Sharin', 'Saraban Tasnim', 'STS', 'Saraban Tasnim Sharin', 'STSharin']);
